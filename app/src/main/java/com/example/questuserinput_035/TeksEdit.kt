860359a0533333463bc765cc68dde4bb71503026
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-31 16:00:19 +0700

    Divider sebagai pemisah antar bagian UI

--- app/src/main/java/com/example/questuserinput_035/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_035/TeksEdit.kt
@@ -84,3 +84,11 @@ fun FormDataDiri(modifier: Modifier
             }
         )
 
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
