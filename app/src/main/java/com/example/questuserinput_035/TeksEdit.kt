ce42d6af6b356fe23278cf49ffb96e440518045d
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-31 15:59:25 +0700

    textfield untuk input alamat lengkap

--- app/src/main/java/com/example/questuserinput_035/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_035/TeksEdit.kt
@@ -74,3 +74,13 @@ fun FormDataDiri(modifier: Modifier
             }
         }
 
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width=250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
